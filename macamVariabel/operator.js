c9fb03e75095428d3d3acf18e1dab350130a8119
mh4nx7net <androxoss@hotmail.com>
2018-12-16 00:51:48 +0700

    add

--- macamVariabel/operator.js
+++ macamVariabel/operator.js
@@ -1,7 +1,6 @@
 const nomer = [1, 2, 3];
-const nobar = [].concat(nomer).concat(6, 5);
-
-nobar.push(4);
-console.log(nomer);
-console.log(nobar.sort());
-console.log(nomer === nobar);
+const nobar = [].concat(nomer).concat(6, 5); /* nobar adalah aray + nomer + [6,5] */
+nobar.push(4); /* nobar push 4 */
+console.log(nomer); /* nomer = 1,2,3 */
+console.log(nobar.sort()); /* nobar = 1,2,3,4,5,6 */
+console.log(nomer === nobar); /* bool nomer=nobar adalah false */
